31ad9c51ea41e9de41096027667c4e161e0abbb3
meshvagupta <meshvagupta18.gnu.ac.in>
2020-03-25 09:48:53 +0530

    Remove directory

--- myapp1/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from myapp1.models import User, User_fb, User_linkedin
-
-admin.site.register(User)
-admin.site.register(User_fb)
-admin.site.register(User_linkedin)
